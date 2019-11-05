489038ed351709015dc73a969ae7414527bd25aa
garryc5 <gcarver994@iwcc.edu>
2019-11-05 08:05:57 -0500

    bug fix on backend

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
 
-app.use('/api/', require('./routes/api'));
+app.use('/api', require('./routes/api'));
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
